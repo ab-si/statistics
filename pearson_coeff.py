7c4fbf32f935053906570365e86e7deb3bac382c
nemo0702 <abhisingh7294@gmail.com>
2019-11-09 16:55:10 +0530

    Pearson Coefficient

---
+++ pearson_coeff.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+'''
+Given two n-element data sets, X and Y,
+calculate the value of the Pearson correlation coefficient.
+'''
+
+def cal_pearson_coeff(X,Y,N):
+    mu_x = sum(X) / N
+    mu_y = sum(Y) / N
+
+    stdv_x = (sum([(i - mu_x)**2 for i in X]) / N)**0.5
+    stdv_y = (sum([(i - mu_y)**2 for i in Y]) / N)**0.5
+    covariance = sum([(X[i] - mu_x) * (Y[i] -mu_y) for i in range(N)])
+
+    correlation_coefficient = covariance / (N * stdv_x * stdv_y)
+    return correlation_coefficient
+
+if __name__ == '__main__':
+    N = int(input())
+    X = list(map(float,input().strip().split()))
+    Y = list(map(float,input().strip().split()))
+    res = cal_pearson_coeff(X,Y,N)
+    print(round(res,3))
